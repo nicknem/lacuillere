3258a6c0a108dd9aa6b284ab1b81bb23899ad7f3
antonin archer <antonin.archer@hec.edu>
2015-08-19 16:32:43 +0200

    add a review works partially

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,21 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.build(review_params)
+    @review.save
+  end
+
+
+  private
+
+  def review_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:review).permit(:content, :rating, :restaurant_id)
   end
 end
