6eae239ada5a4100a7883d1d256a5816013de668
antonin archer <antonin.archer@hec.edu>
2015-08-19 12:04:40 +0200

    wrote validations, not tested yet

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,9 @@
 class Restaurant < ActiveRecord::Base
-  has_many :reviews, dependent: :destroy
+  has_many :reviews, dependent: :destroy # if you destroy a Restaurant it deletes the Review
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"], allow_nil: false}
+
+
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
   belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true, inclusion: { in: [1,2,3,4,5], allow_nil: false }
 end
